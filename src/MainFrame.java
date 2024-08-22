3e6924d46b127db0bef90567089fa2385aa12663
ethan <sample@bths.edu>
2024-08-22 01:57:49 -0400

    randomly generates a painting based on the colors you input

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame implements Runnable {
+    MyGraphics gpanel;
+
+    public MainFrame() {
+        JFrame frame = new JFrame("matice generator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 450);
+        frame.setLocation(100, 50);
+
+        gpanel = new MyGraphics();
+        frame.add(gpanel);
+
+        frame.setVisible(true);
+
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    public void run() {
+        while (true) {
+            gpanel.repaint();
+        }
+    }
 }
--- src/MyGraphics.java
+++ src/MyGraphics.java
@@ -1,43 +1,150 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class Graphics extends JPanel {
-    private BufferedImage ermage;
-    double centerx;
-    double centery;
-    final int R = 30;
-    double theta;
+public class MyGraphics extends JPanel implements ActionListener {
+    private Color[][] myBoard;
+    private ArrayList<Color> colorList;
+    private int look;
+    /*
+    look 1 is the welcome page w rules n shit
+    look 2 is the color selector
+    look 3 is the final product
+     */
+    private JButton look2;
+    private JTextField redInsert;
+    private JTextField greenInsert;
+    private JTextField blueInsert;
+    private JButton enter;
+    private boolean redFilled;
+    private boolean greenFilled;
+    private boolean blueFilled;
+    private String errorMessage;
+    private JButton look3;
 
-
-    public Graphics() {
-        theta = 0;
-        centerx = 200;
-        centery = 50;
-        try {
-            ermage = ImageIO.read(new File("src/stanloona.jpg"));
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+    public MyGraphics() {
+        myBoard = new Color[20][20];
+        colorList = new ArrayList<Color>();
+        look = 1;
+        look2 = new JButton("continue to generator");
+        add(look2);
+        look2.addActionListener(this);
+        redInsert = new JTextField();
+        add(redInsert);
+        redInsert.setVisible(false);
+        redInsert.setColumns(5);
+        greenInsert = new JTextField();
+        add(greenInsert);
+        greenInsert.setVisible(false);
+        greenInsert.setColumns(5);
+        blueInsert = new JTextField();
+        add(blueInsert);
+        blueInsert.setVisible(false);
+        blueInsert.setColumns(5);
+        enter = new JButton("enter");
+        add(enter);
+        enter.addActionListener(this);
+        enter.setVisible(false);
+        redFilled = false;
+        greenFilled = false;
+        blueFilled = false;
+        errorMessage = "";
+        look3 = new JButton("i'm done!");
+        add(look3);
+        look3.addActionListener(this);
+        look3.setVisible(false);
     }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);  // just do this
-        g.setColor(new Color(150, 75, 0)); //brown
-        g.drawLine(80, 80, 50, 50);
-        g.setColor(Color.RED);
-        g.fillRect(50, 100, 40, 20);
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Bodoni MT", Font.BOLD, 18));
-        g.drawString("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 50, 300);
-        theta += 0.01;
-        if (theta >= 2 * Math.PI) {
-            theta = 0;
+        if (look == 1) {
+            g.setFont(new Font("Arial", Font.BOLD, 18));
+            g.drawString("Welcome to the ??? generator !!!", 30, 30);
+            look2.setLocation(30, 40);
+        } else if (look == 2) {
+            g.setFont(new Font("Arial", Font.BOLD, 18));
+            g.drawString("Please enter what colors you'd like to generate with!", 30, 30);
+            g.setFont(new Font("Arial", Font.BOLD, 12));
+            Dimension textSize = redInsert.getPreferredSize();
+            g.drawString("red value:", 30, ((40 + (int) textSize.getHeight()) + 40) / 2);
+            redInsert.setLocation(100, 40);
+            redFilled = redInsert.getText() != null && !blueInsert.getText().equals("");
+            textSize = greenInsert.getPreferredSize();
+            g.drawString("green value:", 30, ((60 + (int) textSize.getHeight()) + 60) / 2);
+            greenInsert.setLocation(110, 60);
+            greenFilled = greenInsert.getText() != null && !greenInsert.getText().equals("");
+            textSize = blueInsert.getPreferredSize();
+            g.drawString("blue value:", 30, ((80 + (int) textSize.getHeight()) + 80) / 2);
+            blueInsert.setLocation(105, 80);
+            blueFilled = blueInsert.getText() != null && !blueInsert.getText().equals("");
+            enter.setLocation(30, 100);
+            g.drawString(errorMessage, 100, 117);
+            look3.setLocation(30, 130);
+            for (int i = 0; i < colorList.size(); i++) {
+                g.setColor(colorList.get(i));
+                g.fillRect(30, 160 + i * 15, 10, 10);
+            }
+        } else if (look == 3) {
+            g.setFont(new Font("Arial", Font.BOLD, 18));
+            g.drawString("here is your painting!", 30, 30);
+            for (int i = 0; i < myBoard.length; i++) {
+                for (int j = 0; j < myBoard[i].length; j++) {
+                    g.setColor(myBoard[i][j]);
+                    g.fillRect(30 + 10 * j, 50 + 10 * i, 10, 10);
+                }
+            }
         }
-        g.drawImage(ermage, (int) (centerx + R * Math.cos(theta)), (int) (centery + R * Math.sin(theta)), null); //moves it in a circle
-        System.out.println(theta);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JButton && e.getSource() == look2) {
+            look = 2;
+            look2.setVisible(false);
+            redInsert.setVisible(true);
+            greenInsert.setVisible(true);
+            blueInsert.setVisible(true);
+            enter.setVisible(true);
+        } else if (e.getSource() instanceof JButton && e.getSource() == enter) {
+            if (!(redFilled && greenFilled && blueFilled)) {
+                errorMessage = "not all fields set!";
+            } else if (!(isInteger(redInsert.getText()) && isInteger(greenInsert.getText()) && isInteger(blueInsert.getText()))) {
+                errorMessage = "please input integers!";
+            } else if (Integer.parseInt(redInsert.getText()) < 0 || Integer.parseInt(redInsert.getText()) > 255 || Integer.parseInt(greenInsert.getText()) < 0 || Integer.parseInt(greenInsert.getText()) > 255 || Integer.parseInt(blueInsert.getText()) < 0 || Integer.parseInt(blueInsert.getText()) > 255) {
+                errorMessage = "please input integers between 0 and 255!";
+            } else {
+                errorMessage = "color successfully added!";
+                colorList.add(new Color(Integer.parseInt(redInsert.getText()), Integer.parseInt(greenInsert.getText()), Integer.parseInt(blueInsert.getText())));
+                look3.setVisible(true);
+            }
+        } else if (e.getSource() instanceof JButton && e.getSource() == look3) {
+            look = 3;
+            look3.setVisible(false);
+            enter.setVisible(false);
+            redInsert.setVisible(false);
+            greenInsert.setVisible(false);
+            blueInsert.setVisible(false);
+            for (int i = 0; i < myBoard.length; i++) {
+                for (int j = 0; j < myBoard[i].length; j++) {
+                    myBoard[i][j] = colorList.get((int) (Math.random() * colorList.size()));
+                }
+            }
+        }
+    }
+
+    //code copied from https://stackoverflow.com/questions/237159/whats-the-best-way-to-check-if-a-string-represents-an-integer-in-java
+    public boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        }
+        catch(NumberFormatException e ) {
+            return false;
+        }
     }
 }
